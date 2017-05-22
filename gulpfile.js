de1d7997709bca5ae9e0f2d38f473eb3e5167d61
Yongseok <hush751001@gmail.com>
2017-05-22 14:00:15 +0900

    /node_modules 를 routes에 추가

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,8 @@ gulp.task('serve', () => {
       server: {
         baseDir: ['.tmp', 'app'],
         routes: {
-          '/bower_components': 'bower_components'
+          '/bower_components': 'bower_components',
+          '/node_modules': 'node_modules',
         }
       }
     });
