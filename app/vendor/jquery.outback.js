0b7bc5e6a19576a5e22a43ad132d4c316d6aa4de
Yongseok <hush751001@gmail.com>
2017-11-07 01:05:19 +0900

    swipe를 외부 library로 대체

--- app/vendor/jquery.outback.js
+++ app/vendor/jquery.outback.js
@@ -2081,7 +2081,6 @@
 
     // setup new event shortcuts
     $.each( ( 'touchstart touchmove touchend ' +
-    'tap taphold ' +
     'scrollstart scrollstop' ).split( ' ' ), function( i, name ) {
 
       $.fn[ name ] = function( fn ) {
@@ -2143,69 +2142,8 @@
       }
     };
 
-    // also handles taphold
-    $.event.special.tap = {
-      tapholdThreshold: 750,
-      emitTapOnTaphold: true,
-      setup: function() {
-        var thisObject = this,
-          $this = $( thisObject ),
-          isTaphold = false;
-
-        $this.bind( 'vmousedown', function( event ) {
-          isTaphold = false;
-          if ( event.which && event.which !== 1 ) {
-            return false;
-          }
-
-          var origTarget = event.target,
-            timer;
-
-          function clearTapTimer() {
-            clearTimeout( timer );
-          }
-
-          function clearTapHandlers() {
-            clearTapTimer();
-
-            $this.unbind( 'vclick', clickHandler )
-              .unbind( 'vmouseup', clearTapTimer );
-            $document.unbind( 'vmousecancel', clearTapHandlers );
-          }
-
-          function clickHandler( event ) {
-            clearTapHandlers();
-
-            // ONLY trigger a 'tap' event if the start target is
-            // the same as the stop target.
-            if ( !isTaphold && origTarget === event.target ) {
-              triggerCustomEvent( thisObject, 'tap', event );
-            } else if ( isTaphold ) {
-              event.preventDefault();
-            }
-          }
-
-          $this.bind( 'vmouseup', clearTapTimer )
-            .bind( 'vclick', clickHandler );
-          $document.bind( 'vmousecancel', clearTapHandlers );
-
-          timer = setTimeout( function() {
-            if ( !$.event.special.tap.emitTapOnTaphold ) {
-              isTaphold = true;
-            }
-            triggerCustomEvent( thisObject, 'taphold', $.Event( 'taphold', { target: origTarget } ) );
-          }, $.event.special.tap.tapholdThreshold );
-        });
-      },
-      teardown: function() {
-        $( this ).unbind( 'vmousedown' ).unbind( 'vclick' ).unbind( 'vmouseup' );
-        $document.unbind( 'vmousecancel' );
-      }
-    };
-
     $.each({
-      scrollstop: 'scrollstart',
-      taphold: 'tap',
+      scrollstop: 'scrollstart'
     }, function( event, sourceEvent ) {
 
       $.event.special[ event ] = {
